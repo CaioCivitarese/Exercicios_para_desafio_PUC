db0d66959fd6dc6101a6030a0eb80ca03a0715bb
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-05-19 17:37:07 -0300

    add

---
+++ Exercicios simulados/Lista de exercicios Seibel 1/Ex 1.py
@@ -0,0 +1,5 @@
+# Jogar “par ou ímpar” com o computador, por 10 iterações, e indicar o que ocorreu mais vezes
+# (você venceu, o computador venceu ou empatou). Deve ser questionada a sua escolha e deve
+# ser gerada aleatoriamente a escolha do computador (números de 0 a 10). O computador escolhe
+# sempre par, assim, se a soma dos números escolhidos for par, o computador venceu, se for
+# ímpar, você venceu.
