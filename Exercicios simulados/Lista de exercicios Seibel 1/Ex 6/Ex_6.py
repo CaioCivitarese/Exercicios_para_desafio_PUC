a4f73bf891a259f932cdf2f031f99c926e33673a
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-05-21 18:52:23 -0300

    add

--- Exercicios simulados/Lista de exercicios Seibel 1/Ex 6/Ex_6.py
+++ Exercicios simulados/Lista de exercicios Seibel 1/Ex 6/Ex_6.py
@@ -2,5 +2,42 @@
 # pertencentes ao intervalo [10,99]. Use a biblioteca de geração de números aleatórios para isso.
 # DICA: Para evitar números repetidos em sua aposta, armazene os números em uma string
 # separados por um espaço e verifique se o número pertence à string.
+import random
 
+nj = 0
+nm = random.randint(10, 99)
 
+while nj != 10:
+    p1 = int(input('Digite seu numero: '))
+    p2 = int(input('Digite seu numero: '))
+    p3 = int(input('Digite seu numero: '))
+    p4 = int(input('Digite seu numero: '))
+    p5 = int(input('Digite seu numero: '))
+    p6 = int(input('Digite seu numero: '))
+
+    if nm == p1:
+        print(p1)
+        print('Participante 1 venceu!!!')
+
+    elif nm == p2:
+        print(p2)
+        print('Participante 2 venceu!!!')
+
+    elif nm == p3:
+        print(p3)
+        print('Participante 3 venceu!!!')
+
+    elif nm == p4:
+        print(p4)
+        print('Participante 4 venceu!!!')
+
+    elif nm == p5:
+        print(p5)
+        print('Participante 5 venceu!!!')
+    elif nm == p6:
+        print(p6)
+        print('Participante 6 venceu!!!')
+    else:
+        print('Inguem venceu')
+    
+    nj = nj + 1
---
+++ Exercicios simulados/Lista de exercicios Seibel 1/Ex 7/Ex_7.py
@@ -0,0 +1,14 @@
+# Elabore um programa que implemente o jogo da forca entre 2 participantes. O primeiro escolhe
+# a palavra secreta e o segundo tenta adivinhar qual é a palavra, O segundo tem N tentativas para
+# adivinhar a palavra onde N = número de letras da palavra secreta.
+# Saída esperada para o programa:
+# Digite a palavra secreta: hipopotamo
+# TENTATIVAS RESTANTES: 10
+# Entre com uma letra: o
+# CERTO!
+# Palavra:
+# h_ _ o_ o_ _ _ o
+# TENTATIVAS RESTANTES: 9
+# Entre com uma letra:... etc
+
+
