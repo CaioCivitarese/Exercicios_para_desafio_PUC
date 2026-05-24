a5faf646fb0f174a4ef0a7b440ba8102a15a4630
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-05-24 18:38:41 -0300

    add

--- Exercicios simulados/Lista de exercicios Seibel 2/Ex 5/ex_5.py
+++ Exercicios simulados/Lista de exercicios Seibel 2/Ex 5/ex_5.py
@@ -2,3 +2,13 @@
 # Considere que o caixa contém notas de R$100,00, de R$50,00, de R$20,00, de R$10,00 e de R$5,00.
 # Indique quantas notas de cada valor serão retiradas.
 
+valor_pagar = int(input('Qual o valor que você que sacar: '))
+
+nota100 = valor_pagar // 100
+nota50 = valor_pagar // 50
+nota20 = valor_pagar // 20
+nota10 = valor_pagar // 10
+nota5 = valor_pagar // 5
+
+if valor_pagar == 5:
+    print()
