394d35d5aba54ecbdc059602e8c1317bc32aebe6
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-07-25 16:36:59 -0300

    add

--- Exercicios Python/Exercicios/Ex 14.py
+++ Exercicios Python/Exercicios/Ex 14.py
@@ -2,4 +2,10 @@
 # das variáveis num1 e num2:
 
 num1 = int(input('Primeiro Valor: '))
+num2 = int(input('Segundo Valor: '))
 
+resolucao1 = num1 * num2
+resolucao2 = num1 / num2
+
+print(resolucao1)
+print(resolucao2)
--- Exercicios Python/Exercicios/Ex 15.py
+++ Exercicios Python/Exercicios/Ex 15.py
@@ -1,1 +1,6 @@
 # 15 - Eleve o valor de num1 a oitava potência, sendo num1 = 51:
+
+num1 = 51
+resolucao = num1 ** 8
+
+print(resolucao)
