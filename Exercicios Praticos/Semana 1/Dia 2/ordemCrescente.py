4622669ff3396afdded160d44a7f035716f6377d
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-08-20 17:23:00 -0300

    add

--- Exercicios Praticos/Semana 1/Dia 2/ordemCrescente.py
+++ Exercicios Praticos/Semana 1/Dia 2/ordemCrescente.py
@@ -1,3 +1,7 @@
 # Leia três números e imprima em ordem crescente.
 
+num1 = int(input('Qual o primeiro numero: '))
+num2 = int(input('Qual o segundo numero: '))
+num3 = int(input('Qual o teceiro numero: '))
+
 
