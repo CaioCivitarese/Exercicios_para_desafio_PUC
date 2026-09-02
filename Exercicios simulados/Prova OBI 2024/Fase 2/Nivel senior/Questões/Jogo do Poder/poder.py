c5ddb8e209f0868af0ace93dee94188b4342bea5
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-09-02 16:54:02 -0300

    add

---
+++ Exercicios simulados/Prova OBI 2024/Fase 2/Nivel senior/Questões/Jogo do Poder/poder.py
@@ -0,0 +1,13 @@
+N = int(input())
+M = int(input())
+I = 0
+
+listaDeNumeros = []
+listaTotal = []
+for i in range(N):
+    for i in range(M):
+        I = int(input())
+        listaDeNumeros.append(I)
+        I = 0
+    listaTotal.append(listaDeNumeros)
+    
