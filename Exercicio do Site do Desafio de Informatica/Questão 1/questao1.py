9d2c4388dfa793d8a6d96f6464b72ae51b9cdb25
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-09-17 15:16:52 -0300

    add

---
+++ Exercicio do Site do Desafio de Informatica/Questão 1/questao1.py
@@ -0,0 +1,7 @@
+# 1) Escreva um programa que leia uma string da entrada padrão e verifique se a string é um palíndromo, ignorando espaços em branco e a diferença entre maiúsculas e minúsculas.
+# Se for um palíndromo, o programa deverá exibir na tela a palavra SIM; caso contrário, deverá exibir NÃO. Considere que a string conterá apenas letras, algarismos e espaços em branco.
+# Definição de palíndromo: sequência de caracteres que pode ser lida da mesma forma de trás para frente quanto de frente para trás. Exemplos: 'osso', '1a2 A2A1'. Se, por exemplo, o programa ler a string 'Am1 1 m a', deverá exibir 'SIM'.
+# Entrada: seu programa receberá da entrada padrão uma string.
+# Saída: para a string de entrada, seu programa deve exibir 'SIM' se for um palíndromo ou 'NÃO', caso contrário.
+
+
---
+++ Exercicio do Site do Desafio de Informatica/Questão 2/questão2.py
@@ -0,0 +1,7 @@
+# 2) Escreva uma função chamada geraPA que receba três números inteiros positivos como parâmetros: • O termo inicial de uma PA (progressão aritmética);
+# • A razão da PA; • A quantidade de termos da PA que a função deve exibir (um em cada linha). Além de exibir na tela os termos da PA, um por linha, ao final, a função também deverá exibir a média dos termos gerados.
+# Lembre-se que um termo da PA é obtido somando-se o termo anterior com a razão. Se, por exemplo, a função receber os valores 3, 2 e 5, deverá exibir as linhas abaixo,
+# onde a última linha corresponde ao somatório dos termos impressos: 3 5 7 9 11 357.0 Implemente também um teste para a sua função. Para tal, o seu programa deve ler da entrada padrão três números inteiros positivos, 
+# representando o termo inicial da PA, sua razão e a quantidade de termos a serem gerados (um número por linha). Para os valores de entrada, o seu programa deve exibir os valores esperados.
+# Entrada: seu programa lerá da entrada padrão três valores inteiros.
+# Saída: para os valores de entrada, seu programa deve exibir os termos da PA e, ao final, o somatório dos termos impressos.
